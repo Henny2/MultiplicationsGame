c288c18abab13bc54de7f2b8cae66d424a38cb93
Henny2 <henrieke.baunack@web.de>
2023-10-28 18:17:45 -0700

    Initial Commit

---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Henrieke Baunack on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
