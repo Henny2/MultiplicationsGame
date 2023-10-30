0747214b7f0d551076c9d65981ee804d3708c7c9
Henny2 <henrieke.baunack@web.de>
2023-10-29 20:24:37 -0700

    adding structs for views, questions, setting up comms between views

--- MultiplicationGame/SessionsView.swift
+++ MultiplicationGame/SessionsView.swift
@@ -6,48 +6,66 @@
 //
 
 import SwiftUI
+import Observation
+
+@Observable
+class ViewModel {
+    init() {}
+    
+    var gameIsActive = false
+    var numQuestions = 5
+    var selectedMultiplication = 0
+    var questions : Array<Question> = []
+}
+
+struct Question {
+    var questionText : String
+    var answer : Int
+}
+
+struct AppContentView: View {
+    var viewModel = ViewModel()
+    var body: some View {
+        return Group{
+            if viewModel.gameIsActive {
+                GameView(viewModel: viewModel)
+            }
+            else {
+                SessionsView(viewModel: viewModel)
+            }
+        }
+    }
+}
 
 struct GameView: View {
-    @Binding var gameIsActive : Bool
-    @Binding var testString : String
+    @Bindable var viewModel: ViewModel
+    @State var questionNumber = 0
+    @State var userAnswer = 0
     var body: some View {
         NavigationStack{
             VStack{
-                Text("Showing some multiplication!")
+                Spacer()
+                Text(viewModel.questions[questionNumber].questionText).font(.title)
+                Spacer()
+                HStack() {
+                    TextField("Answer", value: $userAnswer, format: .number).multilineTextAlignment(.center).font(.title)
+                }
+                Spacer()
+                Spacer()
+                Spacer()
             }
             .navigationTitle("MultiFly")
             .toolbar{
                 Button("Restart Game"){
-                    gameIsActive = false
+                    viewModel.gameIsActive = false
                 }
             }
         }
     }
 }
 
-struct AppContentView: View {
-    @State var gameIsActice = false
-    @State var testString = "Hello"
-    var body: some View {
-        return Group{
-            if gameIsActice {
-                GameView(gameIsActive: $gameIsActice, testString: $testString)
-            }
-            else {
-                SessionsView(gameIsActive: $gameIsActice)
-            }
-        }
-    }
-}
-
 struct SessionsView: View {
-    @State var selectedMultiplication = 1
-    @State var numQuestions = 5
-//    @State private var selectedMultiplication = 0
-//    @State private var numQuestions = 5
-//    @Binding var selectedMultiplication : Int
-//    @Binding var numQuestions : Int
-    @Binding var gameIsActive : Bool
+    @Bindable var viewModel: ViewModel
     let numQuestionsOptions = [5, 10, 15]
     var body: some View {
         NavigationStack{
@@ -55,20 +73,17 @@ struct SessionsView: View {
                 Section("What multiplication do you want to be tested on?") {
                     HStack{
                         Spacer()
-                        Picker("Multiplication Table", selection: $selectedMultiplication) {
+                        Picker("Multiplication Table", selection: $viewModel.selectedMultiplication) {
                             ForEach(1..<13) { num in
                                 Text("\(String(num))")
                             }
                         }
                         .labelsHidden()
-                        //.onReceive([self.selectedMultiplication].publisher.first()) { value in
-                        //    print("value has changed")
-                        //}
                         Spacer()
                     }
                 }
                 Section("How many questions do you want?"){
-                    Picker("Number of questions Picker", selection: $numQuestions){
+                    Picker("Number of questions Picker", selection: $viewModel.numQuestions){
                         ForEach(numQuestionsOptions, id:\.self){ option in
                             Text("\(option)")
                         }
@@ -81,14 +96,32 @@ struct SessionsView: View {
                         Spacer()
                         Button("Yes"){
                             print("reeeeady")
-                            gameIsActive = true
+                            startGame()
+                            viewModel.gameIsActive = true
                         }
                         Spacer()
                     }
                 }
             }.navigationTitle("MultiFly")
         }
-        
+    }
+    func startGame() {
+        // create questions, add them to array
+        viewModel.questions = []
+        for i in 1...viewModel.numQuestions {
+            print("HI \(i)")
+            // create a question
+            // pick a random multiplicator
+            let selectedMultiplicator = viewModel.selectedMultiplication + 1
+            let multiplicator = Int.random(in: 1..<13)
+            let questionText = "What is \(selectedMultiplicator) * \(multiplicator)?"
+            let answer = selectedMultiplicator * multiplicator
+            print(questionText)
+            print(answer)
+            let question = Question(questionText: questionText, answer: answer)
+            viewModel.questions.append(question)
+        }
+        print(viewModel.questions)
     }
 }
 
